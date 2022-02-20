1d9169892a8164a7dabd9aa05c975d0cc216975f
Asaph Bitner <asaphbitner@gmail.com>
2022-02-21 01:18:34 +0200

    2022-02-21 d

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3030;
-const publicPath = path.join(__dirname, '..', './public')
+const publicPath = path.join(__dirname, '/', 'public')
 app.use(express.static(publicPath))
 
 app.listen(port, () => {
